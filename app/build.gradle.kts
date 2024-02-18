5e972aef313ce0cd67759668129aa66a658f7181
jessssa <jessalynlalatasantos07@gmail.com>
2024-02-19 07:26:20 +0800

    strings, build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
  plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-}
+     id("com.google.gms.google-services")
+ }
 
 android {
     namespace = "com.alterpat.emc"
@@ -33,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,6 +45,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
