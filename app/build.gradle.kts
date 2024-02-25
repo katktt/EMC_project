d2f0e6515bf6a1b7a158827a87659056eecdfb05
KAT <iamkat2204@gmail.com>
2024-02-25 20:56:55 +0800

    navigation drawer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
  plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-     id("com.google.gms.google-services")
+    id("com.google.gms.google-services")
  }
 
 android {
@@ -50,5 +50,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.core:core-splashscreen:1.0.0")
-
+    implementation("com.google.android.material:material:1.2.0")
+    implementation("com.google.android.material:material:1.3.0-alpha03")
 }
--- app/src/main/java/com/alterpat/emc/HomePage.kt
+++ app/src/main/java/com/alterpat/emc/HomePage.kt
@@ -2,10 +2,25 @@ package com.alterpat.emc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.FragmentManager
+import com.alterpat.emc.databinding.ActivityMainBinding
 
-class HomePage : AppCompatActivity() {
+class HomePage : AppCompatActivity() NavigationView.OnNavigationItemSelectedListener{
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) { 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean{
+        TODO("Not yet implemented")
+    }
+
+    override fun onBackPressed(){
+        super.onBackPressed()
     }
 }
