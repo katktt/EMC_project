95394886df82b52dbfeea8e89175983eb0c80972
jessssa <jessalynlalatasantos07@gmail.com>
2024-02-26 16:15:47 +0800

    strings, build gradle

--- app/src/main/java/com/alterpat/emc/MainActivity.kt
+++ app/src/main/java/com/alterpat/emc/MainActivity.kt
@@ -3,12 +3,11 @@ package com.alterpat.emc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import android.widget.Button;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(2 500)
+        Thread.sleep(2500)
         installSplashScreen()
         setContentView(R.layout.activity_main)
 
