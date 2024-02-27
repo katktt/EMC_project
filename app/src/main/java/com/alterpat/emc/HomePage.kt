49098f31ff20f25b6c762a9eb272ed7c942a9ffe
jess <iamkat2204@gmail.com>
2024-02-27 17:52:47 +0800

    up

--- app/src/main/java/com/alterpat/emc/HomePage.kt
+++ app/src/main/java/com/alterpat/emc/HomePage.kt
@@ -2,11 +2,15 @@ package com.alterpat.emc.com.alterpat.emc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.alterpat.emc.R
 import com.alterpat.emc.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
@@ -19,11 +23,42 @@ class HomePage : AppCompatActivity(),NavigationView.OnNavigationItemSelectedList
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        val  toggle = ActionBarDrawerToggle(this,binding.draweLayout, binding.toolbar, R.string.nav_open, R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.backround = null
+        binding.bottomNavigation.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_profile -> openFragment(ProfileFragment())
+                R.id.bottom_cart -> openFragment(CartFragment())
+                R.id.bottom_menu -> openFragment(MenuFragment())
+            }
+            true
+        }
+        fragmentManager = supportFragmentManager
+        openFragment(HomePageFragment())
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Categories", Toast.LENGTH_SHORT).show()
+        }
     }
     override fun onNavigationItemSelected(item: MenuItem): Boolean{
         when(item.itemId){
-            
+            R.id.nav_prime -> openFragment(PrimeFragment())
+            R.id.nav_fashion -> openFragment(FashionFragment())
+            R.id.nav_electronics -> openFragment(ElectronicsFragment())
+            R.id.nav_electronics -> Toast.makeText(this, "Fresh", Toast.LENGTH_LONG).show()
+            R.id.nav_electronics -> Toast.makeText(this, "Beauty", Toast.LENGTH_LONG).show()
+            R.id.nav_electronics -> Toast.makeText(this, "Furniture", Toast.LENGTH_LONG).show()
         }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
     override fun onBackPressed() {
         if (binding.drawerlayout.isDrawerOpen(GravityCompat.START)) {
